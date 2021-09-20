42e6726ed7d002fc921cafa4dfc93fa7487d2d4a
praveenak22 <praveenakamireddi22@gmail.com>
2021-09-20 11:04:58 +0530

    sum array added

--- Workspace/sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ Workspace/sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -8,6 +8,12 @@ public class BootClass {
 		show();
 	}
 	public static void show() {
+		int[] arr= {1,2,3}; int sum=0;
+		for(int n:arr)
+		{
+			sum+=arr[n];
+		}
+		System.out.println(sum);
 		System.out.println("Show method");
 	}
 }
