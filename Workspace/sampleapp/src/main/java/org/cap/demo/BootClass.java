59f65be3c1c79aa80d8b5b54eabb367558463ccb
praveenak22 <praveenakamireddi22@gmail.com>
2021-09-20 10:51:08 +0530

    show method added

--- Workspace/sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ Workspace/sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -5,7 +5,9 @@ public class BootClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-
+		show();
 	}
-
+	public static void show() {
+		System.out.println("Show method");
+	}
 }
